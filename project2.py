b4c8f06b9bf4a073fbe5e4205a9256e40f487a8c
Chris Kwiatkowski <chris@chriskski.com>
2017-12-01 21:45:34 -0800

    Add stop words, update intensifiers, add file.lower(), counting for unigram bigram, initial work on trigram

--- project2.py
+++ project2.py
@@ -20,10 +20,23 @@ import nltk
 from nltk import bigrams
 from nltk.collocations import ngrams
 
-NEGATIONS_LIST = \
-["aint", "arent", "cannot", "cant", "couldnt", "darent", "didnt", "doesnt", "ain't", "aren't", "can't", "couldn't", "daren't", "didn't", "doesn't", "dont", "hadnt", "hasnt", "havent", "isnt", "mightnt", "mustnt", "neither", "don't", "hadn't", "hasn't", "haven't", "isn't", "mightn't", "mustn't", "neednt", "needn't", "never", "none", "nope", "nor", "not", "nothing", "nowhere", "oughtnt", "shant", "shouldnt", "uhuh", "wasnt", "werent", "oughtn't", "shan't", "shouldn't", "uh-uh", "wasn't", "weren't", "without", "wont", "wouldnt", "won't", "wouldn't", "rarely", "seldom", "despite"]
+STOP_WORDS = {'has', 'into', 'theirs', 'its', 
+              'being', 'from', 'have', 'were', 'at', 'with', 'my', 'about', 
+              'should', 'did', 'for', 'her', 'their', 'does', 'up', 'had',
+              'yours', 'themselves', 'of', 'been', 'by', 'do', 'where', 'against', 'there', 
+              'which', 'through', 'in', 'other', 'doing',  
+              'those', 'your', 'was', 'hers', 
+              'yourself', 'is', 'his', 
+              'herself', 'an', 'himself', 'you', 
+              'this', 'any', 'the', 'myself', 'ourselves', 'itself', 'that', 'on', 'ours', 
+              'just', 'having', 'are', 'our', 'as', 'they', 'to', 
+              'these', 'both', 'be', 'them', 'yourselves', 'a'}
 
-INTERNSIFIERS_DICT = \
+
+NEGATIONS_SET = \
+{"couldn't", 'nope', "wouldn't", 'hasnt', 'never', "daren't", 'nowhere', 'rarely', "wasn't", 'without', 'neither', 'wouldnt', 'darent', "mustn't", "won't", "haven't", 'arent', 'havent', 'didnt', 'dont', 'wont', 'doesnt', 'couldnt', 'shouldnt', "shouldn't", "shan't", 'wasnt', "aren't", "mightn't", 'cannot', 'werent', 'hadnt', "don't", "weren't", 'not', 'isnt', "hasn't", 'none', "doesn't", "didn't", 'oughtnt', 'shant', 'aint', "needn't", 'uhuh', "oughtn't", "ain't", 'cant', 'despite', 'seldom', 'mightnt', 'neednt', 'uh-uh', 'mustnt', 'nor', "hadn't", "isn't", "can't", 'nothing'}
+
+INTENSIFIER_DICT = \
 {"absolutely": 1.5, "amazingly": 1.5, "awfully": 1.5, "completely": 1.5, "considerably": 1.5,
  "decidedly": 1.5, "deeply": 1.5, "effing": 1.5, "enormously": 1.5,
  "entirely": 1.5, "especially": 1.5, "exceptionally": 1.5, "extremely": 1.5,
@@ -36,11 +49,11 @@ INTERNSIFIERS_DICT = \
  "thoroughly": 1.5, "totally": 1.5, "tremendously": 1.5,
  "uber": 1.5, "unbelievably": 1.5, "unusually": 1.5, "utterly": 1.5,
  "very": 1.5,
- "almost": 0.5, "barely": 0.5, "hardly": 0.5, "just enough": 0.5,
+ "almost": 0.5, "barely": 0.5, "hardly": 0.5,
  "kind of": 0.5, "kinda": 0.5, "kindof": 0.5, "kind-of": 0.5,
- "less": 0.5, "little": 0.5, "marginally": 0.5, "occasionally": 0.5, "partly": 0.5,
+ "less": 0.5, "little": 0.5, "marginally": 0.5, "occasionally": 0.5, "partly": 0.5, "rather": 1.25,
  "scarcely": 0.5, "slightly": 0.5, "somewhat": 0.5,
- "sort of": 0.5, "sorta": 0.5, "sortof": 0.5, "sort-of": 0.5}
+ "sort of": 0.5, "sorta": 0.5, "sortof": 0.5, "sort-of": 0.5, "too": 1.5, "such": 1.5}
 
 # getting CWD for relative filenames
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
@@ -66,7 +79,7 @@ def trivialTokenizer(text):
 def main():
     # clear the screen
     os.system('clear')
-        
+
     print ("Welcome to our second project created by Iurii Shamkin & Chris Kwiatkowski\nThe theme of the project is emotion analysis")
 
 
@@ -98,8 +111,12 @@ def main():
 
     # reading file while replacing new lines and tokenizing into separate sentences
     file = open(user_path_1).read().replace('\n', ' ')
+    file = file.lower()
     sentences = nltk.sent_tokenize(file)
     
+    # emotion_type => (unnegated_count, negated_count)
+    emotion_count = {1: (0,0), 2: (0,0), 3: (0,0), 4: (0,0), 5: (0,0), 6: (0,0), 7: (0,0), 8: (0,0)}
+
     # main loop for extracting unigrams, bigrams and trigrams from each sentence
     for sent in sentences:
         text_unigrams = sent_list = trivialTokenizer(sent)
@@ -107,10 +124,56 @@ def main():
         text_bigrams = list(bigrams(sent_list))
         text_trigrams = list(ngrams(sent_list,3))
 
-        print(text_unigrams)
-        print(text_bigrams)
-        print(text_trigrams)
-        input()
+        
+        for unigram in text_unigrams:
+            if unigram in emotion_lexicon:
+                emotion_types = emotion_lexicon[unigram]
+                for e in emotion_types:
+                    (unnegated_count, negated_count) = emotion_count[e]
+                    emotion_count[e] = (unnegated_count + 1, negated_count)
+
+
+        for bigram in text_bigrams:
+            if bigram[1] in emotion_lexicon:
+                emotion_types = emotion_lexicon[bigram[1]]
+                for e in emotion_types: 
+                    if bigram[0] in NEGATIONS_SET:
+                        # print(bigram)
+                        (unnegated_count, negated_count) = emotion_count[e]
+                        emotion_count[e] = (unnegated_count - 1, negated_count + 1)
+
+                    elif bigram[0] in INTENSIFIER_DICT:
+                        multiplier = INTENSIFIER_DICT[bigram[0]]
+                        (unnegated_count, negated_count) = emotion_count[e]
+                        emotion_count[e] = (unnegated_count - 1 + multiplier, negated_count)
+
+        for trigram in text_trigrams:
+            if trigram[2] in emotion_lexicon:
+                emotion_types = emotion_lexicon[trigram[2]]
+                for e in emotion_types:
+
+                    if trigram[0] in NEGATIONS_SET:
+                        if trigram[1] in STOP_WORDS:
+                            print('neg x e', trigram)
+
+                        # print(trigram)
+                        (unnegated_count, negated_count) = emotion_count[e]
+                        emotion_count[e] = (unnegated_count - 1, negated_count + 1)
+
+                    elif trigram[0] in INTENSIFIER_DICT:
+                        if trigram[1] in STOP_WORDS:
+                            print('int x e', trigram)
+
+                        multiplier = INTENSIFIER_DICT[trigram[0]]
+                        (unnegated_count, negated_count) = emotion_count[e]
+                        emotion_count[e] = (unnegated_count - 1 + multiplier, negated_count)
+
+        # print(text_unigrams)
+        # print(text_bigrams)
+        # print(text_trigrams)
+        # input()
+
+    print(emotion_count)
 
 
 main()
